f96075aeb8a0779cfa4f144cc12b1a1de9b9c956
Krish Agarwal <agarwalkrish29@gmail.com>
2023-08-30 01:50:05 +0530

    userform sorted

--- frontend/src/features/users/NewUserForm.js
+++ frontend/src/features/users/NewUserForm.js
@@ -47,21 +47,19 @@ const NewUserForm = () => {
         }
     }, [isSuccess, navigate])
 
-    const onUsernameChanged = e => setUsername(e.target.value)
-    const onPasswordChanged = e => setPassword(e.target.value)
+    const onUsernameChanged = e => {setUsername(e.target.value)}
+    const onPasswordChanged = e => {setPassword(e.target.value)}
 
     const onRolesChanged = e => {
-        const values = Array.from(
-            e.target.selectedOptions, //HTMLCollection 
-            (option) => option.value
-        )
-        setRoles(values)
+        // console.log(e)
+        
+        setRoles(e)
     }
 
     const canSave = [roles.length, validUsername, validPassword].every(Boolean) && !isLoading
 
     const onSaveUserClicked = async (e) => {
-        e.preventDefault()
+        // e.preventDefault()
         if (canSave) {
             await addNewUser({ username, password, roles })
         }
